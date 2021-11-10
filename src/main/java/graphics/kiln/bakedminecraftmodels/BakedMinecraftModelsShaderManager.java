aeea5b7984d780baa1cb22f53dba88952ebc8c83
burgerguy <ryan.gehling@yahoo.com>
2021-11-10 18:35:18 -0500

    rename some things + update gradle

--- src/main/java/graphics/kiln/bakedminecraftmodels/BakedMinecraftModelsShaderManager.java
+++ src/main/java/graphics/kiln/bakedminecraftmodels/BakedMinecraftModelsShaderManager.java
@@ -8,20 +8,19 @@ package graphics.kiln.bakedminecraftmodels;
 
 import java.io.IOException;
 
-import net.minecraft.client.gl.GlUniform;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.client.render.Shader;
 import net.minecraft.resource.ResourceManager;
 
 public class BakedMinecraftModelsShaderManager {
-    public static @Nullable Shader SMART_ENTITY_CUTOUT_NO_CULL;
-    public static @Nullable Shader SMART_ENTITY_TRANSLUCENT;
+    public static @Nullable Shader ENTITY_CUTOUT_NO_CULL_INSTANCED;
+    public static @Nullable Shader ENTITY_TRANSLUCENT_BATCHED;
 
     public static void loadShaders(ResourceManager manager) {
         try {
-            SMART_ENTITY_CUTOUT_NO_CULL = new Shader(manager, "rendertype_entity_cutout_no_cull_instanced", BakedMinecraftModelsVertexFormats.SMART_ENTITY_FORMAT);
-            SMART_ENTITY_TRANSLUCENT = new Shader(manager, "rendertype_entity_translucent_smart", BakedMinecraftModelsVertexFormats.SMART_ENTITY_FORMAT);
+            ENTITY_CUTOUT_NO_CULL_INSTANCED = new Shader(manager, "rendertype_entity_cutout_no_cull_instanced", BakedMinecraftModelsVertexFormats.SMART_ENTITY_FORMAT);
+            ENTITY_TRANSLUCENT_BATCHED = new Shader(manager, "rendertype_entity_translucent_batched", BakedMinecraftModelsVertexFormats.SMART_ENTITY_FORMAT);
         } catch (IOException e) {
             e.printStackTrace();
         }
